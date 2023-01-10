70d0555d530efd0d6d40a5b2f48a3cb7ad9a59c4
haeun0320 <50322852+haeun0320@users.noreply.github.com>
2023-01-10 17:57:23 +0900

    jar->war

--- poo/src/main/java/com/example/poo/PooApplication.java
+++ poo/src/main/java/com/example/poo/PooApplication.java
@@ -2,9 +2,16 @@ package com.example.poo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PooApplication {
+public class PooApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(PooApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PooApplication.class, args);
